2db78c37b2af9c504cd002992a80ff4ef16b2af2
Tejas032005 <tejaskumarkesharwani168@gmail.com>
2026-02-03 11:49:00 +0530

    Async file read

--- index1.js
+++ index1.js
@@ -1,6 +1,20 @@
 const fs = require('fs');
+// ** sync file
+
 // fs.writeFileSync("./ds-b.txt","Hello ABES COLLEGE Student ");
 // fs.writeFileSync("./abes.txt","Hi abes College student");
- const result = fs.readFileSync("./abes.txt","utf-8")
- console.log(result);
- 
+//  const result = fs.readFileSync("./abes.txt","utf-8")   
+//  console.log(result);
+
+// ** Asyncfile
+//  fs. writeFile("../abes.txt","We are code",() => {});
+fs. readFile("./abes.txt","utf-8",(err,result)=>{
+    if(err){
+        console.log("Error",err);
+        
+    }
+    else{
+        console.log(result);
+        
+    }
+});
